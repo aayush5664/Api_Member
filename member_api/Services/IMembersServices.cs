c12e7a202ff0d74b13f2292bc983c75853989302
Sharma <2065792@cognizant.com>
2022-05-20 15:55:40 +0530

    first commit

---
+++ member_api/Services/IMembersServices.cs
@@ -0,0 +1,16 @@
+﻿using LibraryReact.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LibraryReact.Services
+{
+    public interface IMembersServices
+    {
+        Task<IEnumerable<Member>> GetMembers();
+        Task<Member> GetMemberById(string memberObj);
+        Task<Member> UpdateMember(Member memberObj);
+        Task<Member> CreateMember(Member memberObj);
+        Task<Member> DeleteMember(string memberObj);
+
+    }
+}
